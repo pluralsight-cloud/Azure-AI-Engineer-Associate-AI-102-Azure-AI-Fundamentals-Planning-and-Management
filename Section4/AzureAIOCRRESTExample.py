c43e7de0b7a34f04dfcdcef2eb50c4f2ca86cc44
John Savill <john@savilltech.com>
2024-03-02 05:49:32 -0600

    Made the rest output nicer to understand

--- Section4/AzureAIOCRRESTExample.py
+++ Section4/AzureAIOCRRESTExample.py
@@ -20,7 +20,18 @@ params = {'language': 'en'}
 
 #Call the endpoint with our image
 response = requests.post(ai_url, headers=headers, params=params, json={"url": read_image_url})
-response.raise_for_status()
 
-# Print the response
-print(response.json())
+if response.status_code != 200:
+    # If unsuccessful, print the error message
+    print("Error:\n", response.json())
+else:
+    jsonResponse = response.json()
+
+    # Iterate over all child nodes in the dictionary
+    for lines in jsonResponse["readResult"]["blocks"]:
+        #we have a list
+        for items in lines.items():
+            #We have a tuple
+            for line in items[1]:
+                print(line['text'])
+                print(line['boundingPolygon'])
